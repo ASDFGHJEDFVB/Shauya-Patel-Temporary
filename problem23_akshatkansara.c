3618901078bb3c54a6c4e4139ba4ba81dc9c28ec
akshat-kansara <akshat.kansara.1907@gmail.com>
2026-09-03 09:52:33 +0530

    Add files via upload

---
+++ problem23_akshatkansara.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+    int n1,n2,n3,n4,n5;
+    printf("input the marks of subject 1:");
+    scanf("%d",&n1);
+    printf("input the marks of subject 2:");
+    scanf("%d",&n2);
+    printf("input the marks of subject 3:");
+    scanf("%d",&n3);
+    n4=n1+n2+n3;
+    n5=n4/3;
+    printf("the avg is:%d\n",n5);
+    printf("the total is:%d",n4);
+
+
+
+
+
+
+ }
---
+++ problem24_akshatkansara.c
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+void main()
+{
+    int x,y,temp;
+    printf("enter x:");
+    scanf("%d",&x);
+    printf("enter y:");
+    scanf("%d",&y);
+    temp=x;
+    x=y;
+    y=temp;
+    printf("the swapped values of x and y respectively are:%d%d",x,y);
+
+}
