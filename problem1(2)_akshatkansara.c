271fac3b3a41b4015248d1b8f9e5a0d54eeca45f
akshat-kansara <akshat.kansara.1907@gmail.com>
2026-09-03 10:04:21 +0530

    Add files via upload

---
+++ problem1(2)_akshatkansara.c
@@ -0,0 +1,25 @@
+
+#include<stdio.h>
+void main()
+{
+    int n1,n2;
+    printf("enter the first value:");
+    scanf("%d",&n1);
+    printf("enter the second value:");
+    scanf("%d",&n2);
+    if(n1>n2)
+    {
+        printf("n1 is greater than n2");
+    }
+    else{
+        printf("n2 is greater than n1");
+    }
+
+
+
+
+
+
+
+
+}
---
+++ problem2(2)_akshatkansara.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+void main()
+{
+    int n1,n2,n3;
+    printf("enter the first value:");
+    scanf("%d",&n1);
+    printf("enter the second value:");
+    scanf("%d",&n2);
+    printf("enter the third value:");
+    scanf("%d",&n3);
+    if(n1>n2 && n1>n3)
+    {
+        printf("n1 is greater than n2 and n3");
+    }
+    else if(n2>n1 && n2>n3)
+    {
+                printf("n2 is greater than n1 and n3");
+
+    }
+    else{
+                printf("n3 is greater than n1 and n2");
+    }
+
+
+
+
+
+
+
+
+}
