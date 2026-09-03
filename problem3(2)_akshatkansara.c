199c4467083da046e06bd794d0cd4eab51948d6b
akshat-kansara <akshat.kansara.1907@gmail.com>
2026-09-03 10:35:56 +0530

    Add files via upload

---
+++ problem3(2)_akshatkansara.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+void main()
+{
+    float n,g,a,d;
+    printf("enter the g value:");
+    scanf("%f",&g);
+    if(g>10000)
+    {
+        a=g/10;
+        d=g*0.03;
+        n=g+a-d;
+        printf("the net salary is:%f",n);
+    }
+    else if(g>5000 && g<10000)
+    {
+        a=g*0.07;
+        d=g*0.02;
+        n=g+a-d;
+        printf("the net salary is:%f",n);
+
+    }
+    else{
+
+    }
+
+
+
+
+
+
+
+
+}
+
---
+++ problem4(2)_akshatkansara.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+    int n;
+    printf("enter the n value:");
+    scanf("%d",&n);
+    if(n%7==0)
+    {
+        printf("n is divisible by 7");
+    }
+
+    else
+    {
+        printf("n is not divisible by 7");
+    }
+
+}
